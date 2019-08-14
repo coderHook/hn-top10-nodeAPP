d4833565ef368f1a455712606ca3891a8c8b7fdd
coderhook <abel1987@hotmail.es>
2019-08-14 10:44:36 +0200

    Add 3 endopoint

--- src/routes/top10.routes.ts
+++ src/routes/top10.routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { titlesLast25Controller, inPostLastWeekController } from './../controllers/top10.controller'
+import { titlesLast25Controller, inPostLastWeekController, inTitlesLast600HighKarma } from './../controllers/top10.controller'
 
 const router = express.Router()
 
@@ -12,5 +12,10 @@ router.get('/in-titles-last-25-Stories', titlesLast25Controller)
  * Get the top 10 words in a Post of exactly a week Ago, if today is Monday from last Monday.
  */
 router.get('/in-post-last-week', inPostLastWeekController)
+
+/**
+ * Get the top 10 words from titles from 600 stories of users with Karma > 10000
+ */
+router.get('/in-titles-high-karma', inTitlesLast600HighKarma)
 
 export default router
